36e0497e23a39638c25ec3d72a17a9e9f3ba15e7
Mahmoud Hosni <mah1998mod@gmail.com>
2023-01-24 19:35:02 +0200

    init froms

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## To Create Template Drevin Form 
+ 1- must import FormsModule
+ 2- create Refrance to form tag => #userFrom="ngForm"
+ 3- write ngModel to each input elment in form
+ 4- must add name attribute to each input elment
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularForms';
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel:User = new User(
+    'Mahmoud',
+    'Mah1998mod@gmail.com',
+    123456789,
+    '',
+    'morning',
+    true
+  );
 }
