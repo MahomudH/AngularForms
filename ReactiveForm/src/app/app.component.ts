e316784422af86826e50be0916999df239e49b52
Mahmoud Hosni <mah1998mod@gmail.com>
2023-01-27 20:32:07 +0200

    Managing Control Values_cont

--- ReactiveForm/src/app/app.component.ts
+++ ReactiveForm/src/app/app.component.ts
@@ -7,14 +7,37 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   registraionForm = new FormGroup({
-    usernmae : new FormControl('Mahmoud'),
-    password : new FormControl(''),
-    confirmPassword : new FormControl(''),
-  address:new FormGroup({
-    city:new FormControl(''),
-    state:new FormControl(''),
-    postalCode:new FormControl(''),
-  })
+    usernmae: new FormControl('Mahmoud'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl(''),
+    }),
   });
+
+  loadAPIData(){
+    this.registraionForm.patchValue({
+      usernmae:'Ahmed',
+      password:'123456',
+      confirmPassword:'123456',
+      address:{
+        postalCode:3210546
+      }
+    });
+
+    this.registraionForm.setValue({
+      usernmae:'Ahmed',
+      password:'123456',
+      confirmPassword:'123456',
+      address:{
+        city:'Gaze',
+        state:'palestine',
+        postalCode:3210546
+      }
+    });
+  }
 }
